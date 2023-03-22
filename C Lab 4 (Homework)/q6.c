10f0a4b47296bc36e2aa6f48a13e4245dbb443dc
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-03-22 12:20:40 +0530

    commit

--- C Lab 4 (Homework)/q6.c
+++ C Lab 4 (Homework)/q6.c
@@ -1,18 +1,24 @@
-//p6)Develop a program to display each digit, starting with the rightmost digit. Your program should also determine whether or not the number is divisible by 9.
-#include<stdio.h> 
-int main() {
-int num,sum=0; 
-printf("Enter the number: "); 
-scanf("%d",&num);
- while(num > 0){
-int mod = num % 10; printf("%d\n",mod); sum+=mod;
-num = num / 10;
-}
+// p6)Develop a program to display each digit, starting with the rightmost digit. Your program should also determine whether or not the number is divisible by 9.
+#include <stdio.h>
+int main()
+{
+    int num, sum = 0;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    while (num > 0)
+    {
+        int mod = num % 10;
+        printf("%d\n", mod);
+        sum += mod;
+        num = num / 10;
+    }
 
-if(sum%9==0)
-printf("It is divisible by 9"); else{
-printf("It is not divisible by 9");
-}
+    if (sum % 9 == 0)
+        printf("It is divisible by 9");
+    else
+    {
+        printf("It is not divisible by 9");
+    }
 
-return 0;
+    return 0;
 }
