7f6781140cd195ced7336a137d6a43adf3fdf64d
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-05-10 13:38:11 +0530

    update

---
+++ C Lab 15 Day 17 (Classwork)/q6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int astrlen(char string[2])
+    {
+        int i = 0;
+        while (str[i] != '\0')
+            i++;
+        return i;
+    }
+    return 0;
+}
